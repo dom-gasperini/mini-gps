8d16e71c3393cce01c6012e2bf4473637b00b6a8
dom <dominicgaspeini@gmail.com>
2024-05-03 00:08:37 -0400

    added display config

---
+++ .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/dom/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/dom/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/dom/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/dom/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
